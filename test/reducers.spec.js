3839ffa9fc867ba82641877ccd67ac89e14f6049
Dan Jensen <dj@fitbird.com>
2016-03-26 10:41:57 -0400

    add test of reducers

--- test/reducers.spec.js
+++ test/reducers.spec.js
@@ -3,23 +3,23 @@ import userhabits from '../app/reducers/userhabits'
 
 describe('reducers', () => {
   describe('userhabits', () => {
-    it('should equal initial state', () => {
+    it('should be initial state', () => {
       expect(userhabits(undefined, {})).toEqual({})
     })
     it('should create userhabit', () => {
       const before = {}
       const action = {
         type: 'CREATE_USERHABIT',
-        userhabit: { "_id": "1", "title": "Some habit" }
+        userhabit: { '_id': '1', 'title': 'Some habit' }
       }
-      const after = { 1: { "_id": "1", "title": "Some habit" } }
+      const after = { 1: { '_id': '1', 'title': 'Some habit' } }
       expect(userhabits(before, action)).toEqual(after)
     })
     it('should delete userhabit', () => {
-      const before = { 1: { "_id": "1", "title": "Some habit" } }
+      const before = { 1: { '_id': '1', 'title': 'Some habit' } }
       const action = {
         type: 'DELETE_USERHABIT',
-        userhabit: { "_id": "1", "title": "Some habit" }
+        userhabit: { '_id': '1', 'title': 'Some habit' }
       }
       const after = {}
       expect(userhabits(before, action)).toEqual(after)
