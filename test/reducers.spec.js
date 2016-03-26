f50a673e4228b94586cf0fb137a3552ae9fa10b4
Dan Jensen <dj@fitbird.com>
2016-03-26 11:14:30 -0400

    add test of reducers

--- test/reducers.spec.js
+++ test/reducers.spec.js
@@ -10,16 +10,25 @@ describe('reducers', () => {
       const before = {}
       const action = {
         type: 'CREATE_USERHABIT',
-        userhabit: { '_id': '1', 'title': 'Some habit' }
+        userhabit: { '_id': '1', 'title': 'Some title' }
       }
-      const after = { 1: { '_id': '1', 'title': 'Some habit' } }
+      const after = { 1: { '_id': '1', 'title': 'Some title' } }
+      expect(userhabits(before, action)).toEqual(after)
+    })
+    it('should update userhabit', () => {
+      const before = { 1: { '_id': '1', 'title': 'Some title' } }
+      const action = {
+        type: 'UPDATE_USERHABIT',
+        userhabit: { '_id': '1', 'title': 'Another title' }
+      }
+      const after = { 1: { '_id': '1', 'title': 'Another title' } }
       expect(userhabits(before, action)).toEqual(after)
     })
     it('should delete userhabit', () => {
-      const before = { 1: { '_id': '1', 'title': 'Some habit' } }
+      const before = { 1: { '_id': '1', 'title': 'Some title' } }
       const action = {
         type: 'DELETE_USERHABIT',
-        userhabit: { '_id': '1', 'title': 'Some habit' }
+        userhabit: { '_id': '1', 'title': 'Some title' }
       }
       const after = {}
       expect(userhabits(before, action)).toEqual(after)
