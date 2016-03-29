cc831cc1478c106ca600b7f57289cefc53179744
Dan Jensen <dj@fitbird.com>
2016-03-28 23:29:54 -0400

    update habit actions to use couchbase

--- app/components/HabitList.js
+++ app/components/HabitList.js
@@ -1,7 +1,10 @@
 import React, { Component, ListView, PropTypes, Text, TouchableOpacity, View } from 'react-native'
-import { readHabits } from '../utils/readHabits'
+// import { readHabits } from '../utils/readHabits'
 import ListHeader from '../components/ListHeader'
 import styles from './HabitListStyles'
+import dbUrl from '../constants/dbUrl'
+import headers from '../constants/headers'
+import { readDoc } from '../utils/readDoc'
 
 export default class HabitList extends Component {
   static propTypes = {
@@ -18,11 +21,20 @@ export default class HabitList extends Component {
     this.pressRow = this.pressRow.bind(this)
   }
   componentDidMount() {
-    console.log(readHabits())
-    this.setState({ data: readHabits() })
+    this.setState({ data: this.readHabits() })
     this.props.actions.getHabits()
   }
+  async readHabits() {
+    try {
+      let res = await readDoc(`${dbUrl}/habit`, headers)
+      let json = await res.json()
+      return json
+    } catch(err) {
+      console.error(err)
+    }
+  }
   render() {
+    console.log(this.state.data['_65'])
     const ds = new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2 })
     const dataSource = ds.cloneWithRows(this.props.data.habits || [])
     const header = this.props.data.summary || 'Sorry, you can not select any habits.'
