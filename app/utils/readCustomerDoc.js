e4dd47a626ebf23dcd92d2d7eb0889838085f2e0
Dan Jensen <dj@fitbird.com>
2016-03-31 10:14:30 -0400

    update initialize database

--- app/utils/readCustomerDoc.js
+++
@@ -1,6 +0,0 @@
-import dbUrl from '../constants/dbUrl'
-import { readDoc } from '../utils/readDoc'
-
-export const readCustomerDoc = () => {
-  return readDoc(`${dbUrl}/customer`).then(res => res.json()).then(doc => doc).catch(err => err)
-}
--- app/utils/startHabit.js
+++ app/utils/startHabit.js
@@ -1,15 +1,15 @@
 import dbUrl from '../constants/dbUrl'
+import { readDoc } from '../utils/readDoc'
 import { updateDoc } from '../utils/updateDoc'
 
 export const startHabit = h => {
+  const url = `${dbUrl}/customer`
   const d = new Date()
-  const data = {
-    habits: {
-      title: h.title,
-      started: d.toJSON()
+
+  return readDoc(url).then(res => res.json()).then(doc => {
+    if (doc.ok) {
+      Object.assign({}, doc.habits, { [h.title]: { started: d.toJSON() } })
     }
-  }
-  return updateDoc(`${dbUrl}/customer`, data).then(res => res.json()).then(doc => {
-    return doc
+    return updateDoc(url, doc).then(res => res).catch(err => err)
   }).catch(err => err)
 }
