285e2b4de7e8846bc2ddaf3be1e7534613697c55
Dan Jensen <dj@fitbird.com>
2016-04-01 19:05:40 -0400

    update habit actions

--- app/utils/initializeDatabase.js
+++ app/utils/initializeDatabase.js
@@ -4,15 +4,8 @@ import { readDatabase } from '../utils/readDatabase'
 import { initializeCustomer } from '../utils/initializeCustomer'
 import { initializeHabits } from '../utils/initializeHabits'
 
-// Keep this while developing customer doc
-// import { deleteDoc } from '../utils/deleteDoc'
-// const id = 'customer'
-// const rev = '93-e3dcbd17e2e40daa95ea4ed9e59a18ed'
-
 export const initializeDatabase = () => {
-  // deleteDoc(`${dbUrl}/${id}?rev=${rev}`)
   return readDatabase(dbUrl).then(res => {
-    // initializeCustomer()
     if (res.status !== 200) {
       return createDatabase(dbUrl).then(res => {
         if (res.status === 200) {
