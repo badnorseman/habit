c67bb23d7be42c908c54877f4704e6e445218fde
Dan Jensen <dj@fitbird.com>
2016-04-02 19:32:23 -0400

    update read habits and read customer

--- app/utils/readCustomer.js
+++ app/utils/readCustomer.js
@@ -2,5 +2,5 @@ import dbUrl from '../constants/dbUrl'
 import { readDoc } from '../utils/readDoc'
 
 export const readCustomer = () => {
-  return readDoc(`${dbUrl}/customer`).then(res => res.json()).then(doc => doc).catch(err => err)
+  return readDoc(`${dbUrl}/customer`).then(res => res.json()).catch(err => err)
 }
