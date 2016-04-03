020c0100c3a76576f9c52a44a84bd07112dd8013
Dan Jensen <dj@fitbird.com>
2016-04-03 12:18:29 -0400

    update add points

--- app/utils/addPoints.js
+++ app/utils/addPoints.js
@@ -1,9 +1,9 @@
-export const addPoints = (points = 0, lastChecked) => {
+export const addPoints = (lastChecked) => {
   if (Date.parse(lastChecked)) {
     const today = new Date().getTime()
     const diffDays = Math.round(Math.abs(
       ((new Date(lastChecked).getTime()) - today) / (24 * 3600 * 1000)))
-    return diffDays === 0 ? points : diffDays === 1 ? points + (100*1.1) : points + 100
+    return diffDays === 0 ? 0 : diffDays === 1 ? 100*1.1 : 100
   } else {
     return 100
   }
